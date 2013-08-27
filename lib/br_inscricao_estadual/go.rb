19374b999eb4516af6a5ac549f9afa790aa66173
Yuri Poloni <yuripoloni@gmail.com>
2013-08-27 13:59:54 -0300

    Adicionado testes e corrigido erro na validacao para o estado de goias

--- lib/br_inscricao_estadual/go.rb
+++ lib/br_inscricao_estadual/go.rb
@@ -8,7 +8,7 @@ module BrInscricaoEstadual
       PESO = [9, 8, 7, 6, 5, 4, 3, 2]
       STATE_CODE = ["10", "11", "15"]
       STATE_INSC_SIZE = 9
-      DIGITS_ZERO = [10,11]
+      DIGITS_ZERO = [0]
       
 
       def digit_verify_inscription(result)
@@ -17,18 +17,19 @@ module BrInscricaoEstadual
           return insc_int_arr.last if [1,0].include?(insc_int_arr.last)
         end
 
-
         if self.class::DIGITS_ZERO.include?(result)
-           0 
+          0 
         elsif result == 1
-          (insc_int_arr.join().to_i >= 10103105 && insc_int_arr.join().to_i <= 10119997) ? 1 : 0 
+          insc_int_arr_take = insc_int_arr.take(PESO.size).join().to_i
+          (insc_int_arr_take >= 10103105 && insc_int_arr_take <= 10119997) ? 1 : 0 
         else
           (11 - result%11)
         end
       end
 
       def calculate_verify(peso)
-        digit_verify_inscription(calculate(peso))      
+        result = calculate(peso)%11
+        digit_verify_inscription(result)      
       end
   end
 end
--- spec/br_inscricao_estadual/go_spec.rb
+++ spec/br_inscricao_estadual/go_spec.rb
@@ -6,6 +6,20 @@ describe BrInscricaoEstadual::GO do
 	it "should valid Goias states incription" do
 		insc_est = BrInscricaoEstadual::GO.new('10.987.654-7')
 		insc_est.should be_valid
+		insc_est = BrInscricaoEstadual::GO.new('11094402-0')
+		insc_est.should be_valid
+		insc_est = BrInscricaoEstadual::GO.new('11094402-1')
+		insc_est.should be_valid
+	end
+
+	it "should valid Goias states incription with mod result 0" do
+		insc_est = BrInscricaoEstadual::GO.new('11111111-0')
+		insc_est.should be_valid
+	end
+
+	it "should valid Goias states incription with mod result 1" do
+		insc_est = BrInscricaoEstadual::GO.new('10111110-1')
+		insc_est.should be_valid
 	end
 
 	it "should not valid Goias states incription" do
