140b48b6ac58439584ad89c7643ce323e2419383
Yuri Poloni <yuripoloni@gmail.com>
2013-08-27 14:45:46 -0300

    adicionado validacao estado pernambuco

--- spec/br_inscricao_estadual/pe_spec.rb
+++ spec/br_inscricao_estadual/pe_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 
 describe BrInscricaoEstadual::PE do
 
-	it "should valid Paraiba states incription" do
+	it "should valid PERNAMBUCO states incription" do
 		insc_est = BrInscricaoEstadual::PE.new('0321418-40')
 		insc_est.should be_valid
 	end
 
-	it "should not valid Paraiba states incription" do
+	it "should not valid PERNAMBUCO states incription" do
 		insc_est = BrInscricaoEstadual::PE.new('0321419-40')
 		insc_est.should_not be_valid
 	end
