260c8e37e1c03edebf699a9368cee07b92902730
Yuri Poloni <yuripoloni@gmail.com>
2013-08-28 11:31:26 -0300

    Desduplicado modulos de brasilia e do acre, mudava apenas o state_code

--- lib/br_inscricao_estadual/df.rb
+++ lib/br_inscricao_estadual/df.rb
@@ -1,11 +1,7 @@
 module BrInscricaoEstadual
-  class DF < Common
-    protected
-      PESO = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-      PESO2 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+  class DF < AC
+    protected    
       STATE_CODE = "07"
-      STATE_INSC_SIZE = 13
-      DIGITS_ZERO = [10,11]
       
   end
 end
