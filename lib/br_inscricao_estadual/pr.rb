3659ef086f9de5e46d0b3565067882e42ee1b7b3
Douglas Lise <douglas@wonder.com.br>
2014-04-15 08:56:20 -0300

    Ajustes na validação para PR - removido dígito 1 de DIGITS_ZERO

--- lib/br_inscricao_estadual/pr.rb
+++ lib/br_inscricao_estadual/pr.rb
@@ -4,7 +4,7 @@ module BrInscricaoEstadual
       PESO = [3, 2, 7, 6, 5, 4, 3, 2]
       PESO2 = [4, 3, 2, 7, 6, 5, 4, 3, 2]
       STATE_INSC_SIZE = 10
-      DIGITS_ZERO = [0,1,10,11] 
+      DIGITS_ZERO = [10,11]
 
 	end
 end
--- spec/br_inscricao_estadual/pr_spec.rb
+++ spec/br_inscricao_estadual/pr_spec.rb
@@ -8,6 +8,11 @@ describe BrInscricaoEstadual::PR do
 		insc_est.should be_valid
 	end
 
+	it "should valid Parana states incription " do
+		insc_est = BrInscricaoEstadual::PR.new('801.00913-15')
+		insc_est.should be_valid
+	end
+
 	it "should not valid Parana states incription" do
 		insc_est = BrInscricaoEstadual::PR.new('123.45878-50')
 		insc_est.should_not be_valid
