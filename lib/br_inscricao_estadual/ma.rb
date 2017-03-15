18c9ec17741ff940ca6a4e7bfd1d7e7cc67c9508
Douglas Lise <douglaslise@gmail.com>
2017-03-15 13:51:40 -0300

    Ajustes para MA

--- lib/br_inscricao_estadual/ma.rb
+++ lib/br_inscricao_estadual/ma.rb
@@ -1,8 +1,12 @@
 module BrInscricaoEstadual
 	class MA < Common
+    def calculate_verify(peso)
+      remainder = calculate(peso) % 11
+      self.class::DIGITS_ZERO.include?(remainder) ? 0 : 11 - remainder
+    end
+
     protected
       STATE_CODE = "12"
       DIGITS_ZERO = [0,1]
-
-	end
+  end
 end
--- spec/br_inscricao_estadual/ma_spec.rb
+++ spec/br_inscricao_estadual/ma_spec.rb
@@ -4,8 +4,11 @@ require 'spec_helper'
 describe BrInscricaoEstadual::MA do
 
 	it "should valid Maranhao states incription" do
-		insc_est = BrInscricaoEstadual::MA.new('120000385')
-		insc_est.should be_valid
+    insc_est = BrInscricaoEstadual::MA.new('120000385')
+    insc_est.should be_valid
+
+    insc_est = BrInscricaoEstadual::MA.new('122052161')
+    insc_est.should be_valid
 	end
 
 	it "should not valid Maranhao states incription" do
