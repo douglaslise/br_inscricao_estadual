58518da1fda902b6c748377f803f2b0338d82bc1
Douglas Lise <douglaslise@gmail.com>
2017-05-31 16:53:03 -0300

    Validação para AL - não deve validar o tipo (3o dígito) já que existem inscrições já cadastradas com outros tipos além de 0, 3, 5, 7 e 8

--- spec/br_inscricao_estadual/al_spec.rb
+++ spec/br_inscricao_estadual/al_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 
 describe BrInscricaoEstadual::AL do
 
-	it "should valid Alagoas states incription" do
-		insc_est = BrInscricaoEstadual::AL.new('2 4 0 0 0 0 0 4 8')
-		insc_est.should be_valid
-	end
+  context "should valid Alagoas states incription" do
+    it "one" do
+      insc_est = BrInscricaoEstadual::AL.new('2 4 0 0 0 0 0 4 8')
+      insc_est.should be_valid
+    end
+
+  	it "two" do
+  		insc_est = BrInscricaoEstadual::AL.new('244036977')
+  		insc_est.should be_valid
+  	end
+  end
 
 	it "should not valid Alagoas states incription" do
 		insc_est = BrInscricaoEstadual::AL.new('2 4 0 0 0 0 0 4 D')
