dd595bc06c7efede160a7e3f3498a8dc81d5043d
Rodrigo Rosa <rodrigo@wonder.com.br>
2018-02-22 10:44:03 -0300

    Alterada validação para RJ

--- spec/br_inscricao_estadual/rj_spec.rb
+++ spec/br_inscricao_estadual/rj_spec.rb
@@ -5,8 +5,21 @@ describe BrInscricaoEstadual::RJ do
 
 	it "should valid Rio de Janeiro states incription" do
 		insc_est = BrInscricaoEstadual::RJ.new('99.999.99-3')
-		insc_est.should be_valid
+    insc_est.should be_valid
+
+    insc_est = BrInscricaoEstadual::Base.new('99.999.99-3', 'RJ')
+    insc_est.should be_valid
 	end
+
+  it "should valid Rio de Janeiro states incription" do
+    insc_est = BrInscricaoEstadual::RJ.new('87.350.71-1')
+    insc_est.should be_valid
+  end
+
+  it "should valid Rio de Janeiro states incription" do
+    insc_est = BrInscricaoEstadual::RJ.new('79.529.68.0')
+    insc_est.should be_valid
+  end
 
 	it "should not valid Rio de Janeiro states incription" do
 		insc_est = BrInscricaoEstadual::RJ.new('99.999.99-4')
